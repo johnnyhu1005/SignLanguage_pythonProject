83671dfdb71086f7d2b59befc0644946a4c4b699
johnnyhu1005 <johnnyhu1005@gmail.com>
2024-11-03 14:49:31 +0800

    first commit

--- recognize.py
+++ recognize.py
@@ -8,8 +8,10 @@ import time
 model = tf.keras.models.load_model('gesture_recognition_model.h5')
 
 # 設定手勢數量及名稱
-GESTURES = ["Thank you", "Thank you2", "Bye", "Morning1", "Morning2", "Morning3", "Morning4", "Night",
-            "welcome1", "welcome2", "Friend1", "Friend2", "Yes", "No1", "No2"]
+GESTURES = ["Thank you", "Thank you2", "Bye", "Morning1", "Morning2", "Morning3", "Morning4", "Night", "welcome1", "welcome2",
+            "Friend1", "Friend2", "Yes", "No1", "No2", "Treasure1", "Treasure2", "Treasure3", "Raining1", "Raining2",
+            "Need1", "Need2", "Father1", "Father2", "Sunny1", "Sunny2", "Like1", "Like2", "Eat", "Drink1",
+            "Drink2", "doctor1", "doctor2", "mom1", "mom2", "teacher1", "teacher2", "happy1", "happy2", "angry1"]
 
 # 初始化 MediaPipe 和繪圖工具
 mp_hands = mp.solutions.hands
@@ -27,20 +29,40 @@ gesture_history = []  # 記錄最近偵測的手勢
 error_tolerance = 50  # 容許的最大錯誤數量
 current_errors = 0   # 當前錯誤計數
 
-# 定義各個手勢組合
+# 定義各個手勢順序組合
 morning_sequence = ["Morning1", "Morning2", "Morning3", "Morning4"]  # "Morning" 手勢順序
 goodnight_sequence = ["Night", "Morning3", "Morning4"]  # "GoodNight" 手勢順序
 welcome_sequence = ["welcome1", "welcome2"]
 friend_sequence = ["Friend1", "Friend2"]
+no_sequence = ["No1", "No2"]
+treasure_sequence = ["Treasure1", "Treasure2", "Treasure3"]
+raining_sequence = ["Raining1", "Raining2"]
+need_sequence = ["Need1", "Need2"]
+father_sequence = ["Father1", "Father2"]
+doctor_sequence = ["doctor1", "doctor2"]
+mom_sequence = ["mom1", "mom2"]
+teacher_sequence = ["teacher1", "teacher2"]
+happy_sequence = ["happy1", "happy2"]
+
+# 定義當前的手勢步驟
 current_morning_step = 0  # 當前的 "Morning" 手勢步驟
 current_goodnight_step = 0  # 當前的 "GoodNight" 手勢步驟
 current_welcome_step = 0
 current_friend_step = 0
+current_no_step = 0
+current_treasure_step = 0
+current_raining_step = 0
+current_need_step = 0
+current_father_step = 0
+current_doctor_step = 0
+current_mom_step = 0
+current_teacher_step = 0
+current_happy_step = 0
 
 current_combo = None  # 當前顯示的手勢組合
 
 # 初始化 MediaPipe Hands，允許偵測雙手
-with mp_hands.Hands(min_detection_confidence=0.1, min_tracking_confidence=0.1, max_num_hands=2) as hands:
+with mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.1, max_num_hands=2) as hands:
     while cap.isOpened():
         ret, frame = cap.read()
         if not ret:
@@ -82,7 +104,7 @@ with mp_hands.Hands(min_detection_confidence=0.1, min_tracking_confidence=0.1, m
             prediction = model.predict(np.expand_dims(landmarks, axis=0))
             predicted_gesture = GESTURES[np.argmax(prediction)]
 
-            # 如果手勢偵測到 "Morning" 系列手勢，則檢查其順序
+            # 如果手勢偵測到某系列手勢，則檢查其順序
             if predicted_gesture == morning_sequence[current_morning_step]:
                 current_morning_step += 1  # 如果順序正確，進入下一個步驟
                 current_errors = 0  # 重置錯誤計數
@@ -111,14 +133,95 @@ with mp_hands.Hands(min_detection_confidence=0.1, min_tracking_confidence=0.1, m
                     current_combo = "Friend"
                     combo_display_time = current_time
                     current_friend_step = 0  # 重置步驟
+            elif predicted_gesture == no_sequence[current_no_step]:
+                current_no_step += 1  # 如果順序正確，進入下一個步驟
+                current_errors = 0  # 重置錯誤計數
+                if current_no_step == len(no_sequence):
+                    current_combo = "No"
+                    combo_display_time = current_time
+                    current_no_step = 0  # 重置步驟
+            elif predicted_gesture == treasure_sequence[current_treasure_step]:
+                current_treasure_step += 1  # 如果順序正確，進入下一個步驟
+                current_errors = 0  # 重置錯誤計數
+                if current_treasure_step == len(treasure_sequence):
+                    current_combo = "Treasure"
+                    combo_display_time = current_time
+                    current_treasure_step = 0  # 重置步驟
+            elif predicted_gesture == raining_sequence[current_raining_step]:
+                current_raining_step += 1  # 如果順序正確，進入下一個步驟
+                current_errors = 0  # 重置錯誤計數
+                if current_raining_step == len(raining_sequence):
+                    current_combo = "Raining"
+                    combo_display_time = current_time
+                    current_raining_step = 0  # 重置步驟
+            elif predicted_gesture == need_sequence[current_need_step]:
+                current_need_step += 1  # 如果順序正確，進入下一個步驟
+                current_errors = 0  # 重置錯誤計數
+                if current_need_step == len(need_sequence):
+                    current_combo = "Need"
+                    combo_display_time = current_time
+                    current_need_step = 0  # 重置步驟
+            elif predicted_gesture == father_sequence[current_father_step]:
+                current_father_step += 1  # 如果順序正確，進入下一個步驟
+                current_errors = 0  # 重置錯誤計數
+                if current_father_step == len(father_sequence):
+                    current_combo = "Father"
+                    combo_display_time = current_time
+                    current_father_step = 0  # 重置步驟
+            elif predicted_gesture == doctor_sequence[current_doctor_step]:
+                current_doctor_step += 1  # 如果順序正確，進入下一個步驟
+                current_errors = 0  # 重置錯誤計數
+                if current_doctor_step == len(doctor_sequence):
+                    current_combo = "Doctor"
+                    combo_display_time = current_time
+                    current_doctor_step = 0  # 重置步驟
+            elif predicted_gesture == mom_sequence[current_mom_step]:
+                current_mom_step += 1  # 如果順序正確，進入下一個步驟
+                current_errors = 0  # 重置錯誤計數
+                if current_mom_step == len(mom_sequence):
+                    current_combo = "Mom"
+                    combo_display_time = current_time
+                    current_mom_step = 0  # 重置步驟
+            elif predicted_gesture == teacher_sequence[current_teacher_step]:
+                current_teacher_step += 1  # 如果順序正確，進入下一個步驟
+                current_errors = 0  # 重置錯誤計數
+                if current_teacher_step == len(teacher_sequence):
+                    current_combo = "Teacher"
+                    combo_display_time = current_time
+                    current_teacher_step = 0  # 重置步驟
+            elif predicted_gesture == teacher_sequence[current_teacher_step]:
+                current_teacher_step += 1  # 如果順序正確，進入下一個步驟
+                current_errors = 0  # 重置錯誤計數
+                if current_teacher_step == len(teacher_sequence):
+                    current_combo = "Teacher"
+                    combo_display_time = current_time
+                    current_teacher_step = 0  # 重置步驟
+            elif predicted_gesture == happy_sequence[current_happy_step]:
+                current_happy_step += 1  # 如果順序正確，進入下一個步驟
+                current_errors = 0  # 重置錯誤計數
+                if current_happy_step == len(happy_sequence):
+                    current_combo = "Happy"
+                    combo_display_time = current_time
+                    current_happy_step = 0  # 重置步驟
 
             else:
                 # 偵測到錯誤動作，增加錯誤計數，超過容錯範圍時重置
                 current_errors += 1
                 if current_errors > error_tolerance:
+                    # 重置手勢序列
                     current_morning_step = 0  # 重置 "Morning" 手勢序列
                     current_goodnight_step = 0  # 重置 "GoodNight" 手勢序列
                     current_welcome_step = 0  # 重置 "Welcome" 手勢序列
+                    current_friend_step = 0
+                    current_no_step = 0
+                    current_treasure_step = 0
+                    current_raining_step = 0
+                    current_need_step = 0
+                    current_father_step = 0
+                    current_doctor_step = 0
+                    current_mom_step = 0
+                    current_teacher_step = 0
+                    current_happy_step = 0
                     current_errors = 0  # 重置錯誤計數
 
             # 畫出預測結果
--- train_model.py
+++ train_model.py
@@ -1,11 +1,17 @@
 import os
 import numpy as np
 import tensorflow as tf
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+import seaborn as sns
 
 # 載入數據資料夾
 DATA_DIR = "data"
-GESTURES = ["Thank you", "Thank you2", "Bye", "Morning1", "Morning2", "Morning3", "Morning4", "Night",
-            "welcome1", "welcome2", "Friend1", "Friend2", "Yes", "No1", "No2"]  # 與 main.py 中相同
+# 設定手勢數量及名稱
+GESTURES = ["Thank you", "Thank you2", "Bye", "Morning1", "Morning2", "Morning3", "Morning4", "Night", "welcome1", "welcome2",
+            "Friend1", "Friend2", "Yes", "No1", "No2", "Treasure1", "Treasure2", "Treasure3", "Raining1", "Raining2",
+            "Need1", "Need2", "Father1", "Father2", "Sunny1", "Sunny2", "Like1", "Like2", "Eat", "Drink1",
+            "Drink2", "doctor1", "doctor2", "mom1", "mom2", "teacher1", "teacher2","happy1", "happy2", "angry1"]
 
 X, y = [], []
 
@@ -25,7 +31,11 @@ for idx, gesture in enumerate(GESTURES):
 X = np.array(X)
 y = np.array(y)
 
-# 建立 TensorFlow 模型，輸入維度固定為 126（21 個手勢點，3 個坐標，2 隻手的數據）
+# 分割訓練和測試數據
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# 建立 TensorFlow 模型
 model = tf.keras.models.Sequential([
     tf.keras.layers.Input(shape=(126,)),  # 21個點 x 3個座標 x 2隻手
     tf.keras.layers.Dense(128, activation='relu'),
@@ -35,9 +45,38 @@ model = tf.keras.models.Sequential([
 
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-# 訓練模型
-model.fit(X, y, epochs=50)
+# 訓練模型並保存訓練過程的歷史數據
+history = model.fit(X_train, y_train, epochs=50, validation_data=(X_test, y_test))
 
 # 保存模型
 model.save('gesture_recognition_model.h5')
 print("手勢模型已保存")
+
+# 繪製訓練和驗證的損失與準確度曲線
+plt.figure(figsize=(12, 4))
+plt.subplot(1, 2, 1)
+plt.plot(history.history['loss'], label='Training Loss')
+plt.plot(history.history['val_loss'], label='Validation Loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.title('Training and Validation Loss')
+
+plt.subplot(1, 2, 2)
+plt.plot(history.history['accuracy'], label='Training Accuracy')
+plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.title('Training and Validation Accuracy')
+plt.show()
+
+# 預測測試集並生成混淆矩陣
+y_pred = np.argmax(model.predict(X_test), axis=1)
+cm = confusion_matrix(y_test, y_pred)
+plt.figure(figsize=(12, 10))
+sns.heatmap(cm, annot=True, fmt="d", cmap="Blues", xticklabels=GESTURES, yticklabels=GESTURES)
+plt.xlabel("Predicted Labels")
+plt.ylabel("True Labels")
+plt.title("Confusion Matrix")
+plt.show()
